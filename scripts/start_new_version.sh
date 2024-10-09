f400361d78b0e1b31a1bb4bfba243a1503466861
waylon <1158341873@qq.com>
2024-10-09 12:58:32 +0800

    feat: release 1.8.1 #31

--- scripts/start_new_version.sh
+++ scripts/start_new_version.sh
@@ -4,17 +4,15 @@ RELEASE_VERSION=$1
 C_OS="$(uname)"
 MAC_OS="Darwin"
 
-ver=$(grep 'app_version:' app_desc_ce.yaml | cut -c 14-)
+ver=$(grep 'app_version:' app_desc.yaml | cut -c 14-)
 echo "current version: ${ver}"
 
 # version num replace
 if [ "$C_OS" == "$MAC_OS" ];then
     echo "app_version: \"${ver}\""
-    sed -i.bak "s/app_version: ${ver}/app_version: \"${RELEASE_VERSION}\"/" app_desc_ce.yaml
-    sed -i.bak "s/app_version: ${ver}/app_version: \"${RELEASE_VERSION}\"/" app_desc_sg.yaml
-    rm app_desc_ce.yaml.bak app_desc_sg.yaml.bak
+    sed -i.bak "s/app_version: ${ver}/app_version: \"${RELEASE_VERSION}\"/" app_desc.yaml
+    rm app_desc.yaml.bak
 else
-    sed -i "s/app_version: ${ver}/app_version: \"${RELEASE_VERSION}\"/" app_desc_ce.yaml
-    sed -i "s/app_version: ${ver}/app_version: \"${RELEASE_VERSION}\"/" app_desc_sg.yaml
+    sed -i "s/app_version: ${ver}/app_version: \"${RELEASE_VERSION}\"/" app_desc.yaml
 fi
 
