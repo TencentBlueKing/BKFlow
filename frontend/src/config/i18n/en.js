72a223dd5924288fc5b8d0c99bcc45f49cc4f7b3
v_xugzhou <941071842@qq.com>
2025-04-14 16:54:56 +0800

    fix: 插件使用范围·接口格式调整联调 --story=123407520 # Reviewed, transaction id: 38686

--- frontend/src/config/i18n/en.js
+++ frontend/src/config/i18n/en.js
@@ -957,6 +957,7 @@ const en = {
   '是否复制该流程？': 'Do you want to copy this process?',
   '注意：关联的 mock 数据不会同步复制，暂不支持复制带有决策表节点的流程': 'Note: Associated mock data will not be copied, and copying processes with decision table nodes is not supported.',
   '流程复制成功！': 'Process copied successfully!',
+  使用范围编辑成功: 'Scope of Use Edited Successfully',
 };
 
 export default en;
