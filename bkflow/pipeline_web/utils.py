947b00cded49fe89a19265d4bb76a9dad1b9ccdf
ZC-A <1483681501@qq.com>
2025-04-24 14:46:51 +0800

    fix: 变量赋值节点 #82

--- bkflow/pipeline_web/utils.py
+++ bkflow/pipeline_web/utils.py
@@ -17,10 +17,13 @@ We undertake not to change the open source license (MIT license) applicable
 
 to the current version of the project delivered to anyone in the future.
 """
+import logging
 
 from bkflow.pipeline_plugins.components.collections.value_assign.v1_0_0 import (
     ValueAssignComponent,
 )
+
+logger = logging.getLogger("root")
 
 
 def topology_sort(relations):
@@ -53,12 +56,15 @@ def pre_handle_value_assign_pipeline_tree(pipeline_tree):
     变量赋值节点 pipeline 特殊处理 将变量名添加引用符号避免计算被去除
     :params pipeline_tree 流程树
     """
-    for pipeline_node in pipeline_tree["activities"].values():
-        if pipeline_node["component"]["code"] == ValueAssignComponent.code:
-            data = pipeline_node["component"]["data"]
-            for assignment in data["bk_assignment_list"]["value"]:
-                original_key = assignment["key"]
-                assignment["key"] = f"${{{{{original_key}}}}}"
+    try:
+        for pipeline_node in pipeline_tree["activities"].values():
+            if pipeline_node["component"]["code"] == ValueAssignComponent.code:
+                data = pipeline_node["component"]["data"]
+                for assignment in data["bk_assignment_list"]["value"]:
+                    original_key = assignment["key"]
+                    assignment["key"] = f"${{{{{original_key}}}}}"
+    except KeyError as e:
+        logger.error(f"pipeline_tree 格式不统一 {str(e)} 跳过赋值节点预处理")
 
 
 def post_handle_value_assign_pipeline_tree(pipeline_tree):
@@ -66,11 +72,14 @@ def post_handle_value_assign_pipeline_tree(pipeline_tree):
     变量赋值节点 pipeline 特殊处理 将变量名去除引用符号还原
     :params pipeline_tree 流程树
     """
-    for pipeline_node in pipeline_tree["activities"].values():
-        if pipeline_node["component"]["code"] == ValueAssignComponent.code:
-            data = pipeline_node["component"]["data"]
-            for assignment in data["bk_assignment_list"]["value"]:
-                key = assignment["key"]
-                if key.startswith("${{") and key.endswith("}}"):
-                    original_key = key[3:-2]  # 会有多层包括 e.x ${{key}}
-                    assignment["key"] = original_key
+    try:
+        for pipeline_node in pipeline_tree["activities"].values():
+            if pipeline_node["component"]["code"] == ValueAssignComponent.code:
+                data = pipeline_node["component"]["data"]
+                for assignment in data["bk_assignment_list"]["value"]:
+                    key = assignment["key"]
+                    if key.startswith("${{") and key.endswith("}}"):
+                        original_key = key[3:-2]  # 会有多层包括 e.x ${{key}}
+                        assignment["key"] = original_key
+    except KeyError as e:
+        logger.error(f"pipeline_tree 格式不统一 {str(e)} 跳过赋值节点后处理")
