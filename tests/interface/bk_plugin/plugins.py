636e2aa6392d91c4d879971b59c9313fb3f40f98
jackvideo <13226110808@163.com>
2025-04-25 17:53:32 +0800

    feat: 蓝鲸插件二次授权单测补充 #132 (#197)

---
+++ tests/interface/bk_plugin/plugins.py
@@ -0,0 +1,31 @@
+remote_plugin_one = {
+    "plugin": {
+        "id": "123",
+        "region": "ieod",
+        "name": "plugin1",
+        "code": "plugin1",
+        "logo_url": "https://xxx.com/default.png",
+        "creator": "test_user1",
+        "created": "2024-04-09 14:37:34",
+        "updated": "2024-11-06 16:24:47",
+        "tag_info": {"id": 1, "name": "测试插件", "code_name": "test", "priority": 0},
+    },
+    "deployed_statuses": {"stag": {"deployed": True}, "prod": {"deployed": True}},
+    "profile": {"introduction": "测试插件一", "contact": "test_user1,test_user2", "tag": 1},
+}
+
+remote_plugin_two = {
+    "plugin": {
+        "id": "1234",
+        "region": "ieod",
+        "name": "test-plugin-2",
+        "code": "test-plugin-2",
+        "logo_url": "https://xxx.com/default.png",
+        "creator": "test_user1",
+        "created": "2024-04-09 14:37:34",
+        "updated": "2024-11-06 16:24:47",
+        "tag_info": {"id": 1, "name": "测试插件", "code_name": "test", "priority": 0},
+    },
+    "deployed_statuses": {"stag": {"deployed": True}, "prod": {"deployed": True}},
+    "profile": {"introduction": "测试插件二", "contact": "test_user1,test_user2", "tag": 1},
+}
