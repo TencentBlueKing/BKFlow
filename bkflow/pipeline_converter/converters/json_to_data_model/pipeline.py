a6a3f654acc3f122dd7374320b08fc5d8e60f3e3
guohelu <141622458+guohelu@users.noreply.github.com>
2025-04-29 19:01:16 +0800

    feat: 实现gateway协议json到data_model的转换 #184 (#206)
    * feat: 实现gateway协议json到data_model的转换 #184
    # Reviewed, transaction id: 40866
    
    * fix: 修改converters模块为惰性加载 #184
    # Reviewed, transaction id: 40909
    
    * fix: 修改converters模块加载方式 #184
    # Reviewed, transaction id: 40971
    
    * fix: 修复循环引用报错问题 #184
    # Reviewed, transaction id: 40993

--- bkflow/pipeline_converter/converters/json_to_data_model/pipeline.py
+++ bkflow/pipeline_converter/converters/json_to_data_model/pipeline.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 from bkflow.pipeline_converter.constants import NodeTypes
 from bkflow.pipeline_converter.converters.base import JsonToDataModelConverter
+from bkflow.pipeline_converter.converters.json_to_data_model.gateway import (
+    ConditionalParallelGatewayConverter,
+    ConvergeGatewayConverter,
+    ExclusiveGatewayConverter,
+    ParallelGatewayConverter,
+)
 from bkflow.pipeline_converter.converters.json_to_data_model.node import (
     ComponentNodeConverter,
     EndNodeConverter,
     StartNodeConverter,
 )
 from bkflow.pipeline_converter.data_models import Pipeline
-from bkflow.pipeline_converter.hub import CONVERTER_HUB
+from bkflow.pipeline_converter.hub import ConverterHub
 
 
 class PipelineConverter(JsonToDataModelConverter):
-
     def convert(self) -> dict:
         """
         将 json 转换成 DataModel
@@ -24,9 +29,13 @@ class PipelineConverter(JsonToDataModelConverter):
             NodeTypes.COMPONENT.value: ComponentNodeConverter.__name__,
             NodeTypes.END_EVENT.value: EndNodeConverter.__name__,
             NodeTypes.START_EVENT.value: StartNodeConverter.__name__,
+            NodeTypes.PARALLEL_GATEWAY.value: ParallelGatewayConverter.__name__,
+            NodeTypes.EXCLUSIVE_GATEWAY.value: ExclusiveGatewayConverter.__name__,
+            NodeTypes.CONDITIONAL_PARALLEL_GATEWAY.value: ConditionalParallelGatewayConverter.__name__,
+            NodeTypes.CONVERGE_GATEWAY.value: ConvergeGatewayConverter.__name__,
         }
         for node in self.source_data.get("nodes", []):
-            converter_cls = CONVERTER_HUB.get_converter_cls(
+            converter_cls = ConverterHub.get_converter_cls(
                 source=self.source, target=self.target, converter_name=node_type_converter_cls_name_map[node["type"]]
             )
             target_nodes.append(converter_cls(node).convert())
