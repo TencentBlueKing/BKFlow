c2ceb05837c96e65152be1662c5a7129b4210c61
dengyh <dengyh071@gmail.com>
2025-07-05 17:27:28 +0800

    fix: 去除json豁免

--- config/default.py
+++ config/default.py
@@ -181,7 +181,6 @@ MAKO_SANDBOX_IMPORT_MODULES = {
     "random": "random",
     "time": "time",
     "os.path": "os.path",
-    "json": "json",
 }
 BambooSettings.MAKO_SANDBOX_IMPORT_MODULES = MAKO_SANDBOX_IMPORT_MODULES
 # 支持 mako 表达式在 dict/list/tuple 情况下嵌套索引
