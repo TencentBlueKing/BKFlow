47eb0221ba1bf832f5f9a5c8cc4dd4d612bdaccc
guohelu <19503896967@163.com>
2025-08-27 15:59:35 +0800

    feat: 任务列表返回新增任务来源字段 --story=126641235 # Reviewed, transaction id: 55760

--- bkflow/apigw/views/create_mock_task.py
+++ bkflow/apigw/views/create_mock_task.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 TencentBlueKing is pleased to support the open source community by making
 蓝鲸流程引擎服务 (BlueKing Flow Engine Service) available.
@@ -27,6 +26,7 @@ from django.views.decorators.http import require_POST
 
 from bkflow.apigw.decorators import check_jwt_and_space, return_json_response
 from bkflow.apigw.serializers.task import CreateMockTaskWithTemplateIdSerializer
+from bkflow.constants import TaskTriggerMethod
 from bkflow.contrib.api.collections.task import TaskComponentClient
 from bkflow.exceptions import ValidationError
 from bkflow.template.models import Template
@@ -81,6 +81,7 @@ def create_mock_task(request, space_id):
             "pipeline_tree": template.pipeline_tree,
             "mock_data": ser.validated_data["mock_data"],
             "create_method": "MOCK",
+            "trigger_method": TaskTriggerMethod.api.name,
         }
     )
     DEFAULT_NOTIFY_CONFIG = {
