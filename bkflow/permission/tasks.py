3a13d639493215f278d31741e042313ec828a4a4
dengyh <dengyh071@gmail.com>
2025-09-03 17:12:58 +0800

    fix: 修复token清理任务执行失败的问题 --bug=147632033 # Reviewed, transaction id: 56540

--- bkflow/permission/tasks.py
+++ bkflow/permission/tasks.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 TencentBlueKing is pleased to support the open source community by making
 蓝鲸流程引擎服务 (BlueKing Flow Engine Service) available.
@@ -17,6 +16,8 @@ We undertake not to change the open source license (MIT license) applicable
 
 to the current version of the project delivered to anyone in the future.
 """
+import datetime
+
 from blueapps.contrib.celery_tools.periodic import periodic_task
 from celery.schedules import crontab
 from django.conf import settings
@@ -29,4 +30,6 @@ from bkflow.permission.models import Token
 def delete_expired_token():
     # 假设token的过期时间为一小时，一天有2000个用户操作了100个流程，会生成大概200000个token
     # 此时的批量删除的数量级大概是可以接受的
-    Token.objects.filter(expired_time__lt=timezone.now() - settings.TOKEN_RETENTION_TIME).delete()
+    Token.objects.filter(
+        expired_time__lt=timezone.now() - datetime.timedelta(seconds=settings.TOKEN_RETENTION_TIME)
+    ).delete()
