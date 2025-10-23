853b882789294ea2742480a091c796a8c8581707
guohelu <141622458+guohelu@users.noreply.github.com>
2025-10-23 15:12:31 +0800

    Revert "fix: 修复插件问题 --story=127611598 # Reviewed, transaction id: 61643"
    This reverts commit f03342555818f6683512c7bb61bf92977515f7db.

--- bkflow/pipeline_plugins/components/collections/subprocess_plugin/v1_0_0.py
+++ bkflow/pipeline_plugins/components/collections/subprocess_plugin/v1_0_0.py
@@ -27,6 +27,7 @@ from pydantic import BaseModel
 from bkflow.constants import TaskOperationSource, TaskOperationType, TaskTriggerMethod
 from bkflow.contrib.api.collections.interface import InterfaceModuleClient
 from bkflow.exceptions import ValidationError
+from bkflow.task.models import TaskOperationRecord
 
 
 class Subprocess(BaseModel):
@@ -47,11 +48,7 @@ class SubprocessPluginService(Service):
         ]
 
     def execute(self, data, parent_data):
-        from bkflow.task.models import (
-            TaskFlowRelation,
-            TaskInstance,
-            TaskOperationRecord,
-        )
+        from bkflow.task.models import TaskFlowRelation, TaskInstance
         from bkflow.task.operations import TaskOperation
         from bkflow.task.utils import extract_extra_info
 
