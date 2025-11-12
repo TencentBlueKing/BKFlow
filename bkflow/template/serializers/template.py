2dfd90c1397e060195b64e842db2cce18ac02a52
guohelu <19503896967@163.com>
2025-11-12 14:19:18 +0800

    feat: 流程模板增加版本管理功能 --story=128050351 # Reviewed, transaction id: 64146

--- bkflow/template/serializers/template.py
+++ bkflow/template/serializers/template.py
@@ -36,7 +36,7 @@ from bkflow.constants import (
 )
 from bkflow.permission.models import TEMPLATE_PERMISSION_TYPE, Token
 from bkflow.pipeline_web.preview_base import PipelineTemplateWebPreviewer
-from bkflow.space.configs import TemplateTriggerConfig
+from bkflow.space.configs import FlowVersioning, TemplateTriggerConfig
 from bkflow.space.models import Space, SpaceConfig
 from bkflow.template.models import (
     Template,
@@ -136,7 +136,8 @@ class TemplateSerializer(serializers.ModelSerializer):
     @transaction.atomic()
     def create(self, validated_data):
         pipeline_tree = validated_data.pop("pipeline_tree", None)
-        snapshot = TemplateSnapshot.create_snapshot(pipeline_tree)
+        username = self.context["request"].user.username
+        snapshot = TemplateSnapshot.create_snapshot(pipeline_tree, validated_data["space_id"], username)
         validated_data["snapshot_id"] = snapshot.id
         template = super().create(validated_data)
 
@@ -166,10 +167,14 @@ class TemplateSerializer(serializers.ModelSerializer):
             logger.exception("TemplateSerializer update error, err = {}".format(e))
             raise serializers.ValidationError(detail={"msg": ("更新失败,{}".format(e))})
         pre_pipeline_tree = instance.pipeline_tree
-        snapshot = TemplateSnapshot.create_snapshot(pipeline_tree)
-        instance.snapshot_id = snapshot.id
-        snapshot.template_id = instance.id
-        snapshot.save(update_fields=["template_id"])
+        username = self.context["request"].user.username
+        if SpaceConfig.get_config(space_id=instance.space_id, config_name=FlowVersioning.name) == "true":
+            instance.update_draft_snapshot(pipeline_tree, username)
+        else:
+            snapshot = TemplateSnapshot.create_snapshot(pipeline_tree, instance.space_id, username)
+            instance.snapshot_id = snapshot.id
+            snapshot.template_id = instance.id
+            snapshot.save(update_fields=["template_id"])
         instance = super().update(instance, validated_data)
         # 批量修改流程绑定的触发器:
         try:
@@ -303,3 +308,17 @@ class TemplateCopySerializer(serializers.Serializer):
     desc = serializers.CharField(help_text=_("描述"), max_length=256, required=False)
     space_id = serializers.IntegerField(help_text=_("空间ID"), required=True)
     copy_subprocess = serializers.BooleanField(help_text=_("是否复制子流程"), required=False, default=False)
+
+
+class TemplateReleaseSerializer(serializers.Serializer):
+    version = serializers.CharField(help_text=_("版本号"), required=True)
+    desc = serializers.CharField(help_text=_("描述"), required=False)
+
+
+class TemplateSnapshotSerializer(serializers.ModelSerializer):
+    create_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S%z")
+    update_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S%z")
+
+    class Meta:
+        model = TemplateSnapshot
+        fields = ["id", "create_time", "update_time", "version", "template_id", "desc", "draft", "creator", "operator"]
