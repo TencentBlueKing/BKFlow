9dac16bd2592ab0cf6050d49f88c731d9a71c093
dengyh <dengyh071@gmail.com>
2025-11-13 16:19:43 +0800

    refactor: get_task_list接口支持更多过滤参数 --story=128612615

--- bkflow/template/serializers/template.py
+++ bkflow/template/serializers/template.py
@@ -300,6 +300,6 @@ class PreviewTaskTreeSerializer(serializers.Serializer):
 class TemplateCopySerializer(serializers.Serializer):
     template_id = serializers.IntegerField(help_text=_("模板ID"), required=True)
     name = serializers.CharField(help_text=_("模版名称"), max_length=MAX_LEN_OF_TEMPLATE_NAME, required=False)
-    desc = serializers.CharField(help_text=_("描述"), max_length=256, required=False)
+    desc = serializers.CharField(help_text=_("描述"), max_length=256, required=False, allow_blank=True)
     space_id = serializers.IntegerField(help_text=_("空间ID"), required=True)
     copy_subprocess = serializers.BooleanField(help_text=_("是否复制子流程"), required=False, default=False)
