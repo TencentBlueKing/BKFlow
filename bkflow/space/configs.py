16c6b60bfd9a0a22d9c1560ed69bcec62d5015eb
dengyh <dengyh071@gmail.com>
2025-12-30 20:46:35 +0800

    feat: 支持创建任务时, 传入凭证来替换API插件的凭证 --story=129665864

--- bkflow/space/configs.py
+++ bkflow/space/configs.py
@@ -283,6 +283,7 @@ class UniformApiConfig(BaseSpaceConfig):
                 "meta_apis": "{meta_apis url}",
                 "api_categories": "{api_categories url}",
                 "display_name": "{display_name}",
+                "headers": {"X-Custom-Header": "${_system.operator}"},
             }
         }
     }
@@ -450,6 +451,7 @@ class ApiModel(BaseModel):
     meta_apis: str
     api_categories: str
     display_name: str
+    headers: Optional[dict] = None
 
     def get(self, field_name, default=None):
         # 由于获取插件种类/列表时候传入的 key 不确定 需要提供一个 get 方法
