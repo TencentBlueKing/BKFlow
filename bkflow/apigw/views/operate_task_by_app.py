e3a9085ee402a914651b60ab8978e3722f5114fd
dengyh <dengyh071@gmail.com>
2026-01-08 20:08:05 +0800

    feat: 通过绑定应用授权的接口都改成用户认证的 --story=129842516

--- bkflow/apigw/views/operate_task_by_app.py
+++ bkflow/apigw/views/operate_task_by_app.py
@@ -16,15 +16,12 @@ We undertake not to change the open source license (MIT license) applicable
 
 to the current version of the project delivered to anyone in the future.
 """
-import json
-
 from apigw_manager.apigw.decorators import apigw_require
 from blueapps.account.decorators import login_exempt
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from bkflow.apigw.decorators import check_task_bk_app_code, return_json_response
-from bkflow.apigw.serializers.task import OperateTaskSerializer
 from bkflow.contrib.api.collections.task import TaskComponentClient
 from bkflow.utils.trace import CallFrom, append_attributes, start_trace
 
@@ -39,18 +36,17 @@ def operate_task_by_app(request, task_id, operation):
     """
     通过 bk_app_code 权限校验执行任务操作
     请求方的 bk_app_code 需要与任务所属模板绑定的 bk_app_code 一致
+    操作人从网关认证的用户信息中获取
     """
-    data = json.loads(request.body)
-    ser = OperateTaskSerializer(data=data)
-    ser.is_valid(raise_exception=True)
-
     # space_id 已经在装饰器中挂载到 request 上
     space_id = request.space_id
+    # 从网关认证的用户信息中获取操作人
+    operator = request.user.username
 
     with start_trace(
         "operate_task_interface", True, space_id=space_id, task_id=task_id, call_from=CallFrom.APIGW.value
     ):
         append_attributes({"operation": operation})
         client = TaskComponentClient(space_id=space_id)
-        result = client.operate_task(task_id, operation, data=ser.data)
+        result = client.operate_task(task_id, operation, data={"operator": operator})
         return result
