ae05be9cf1551a3820eab1bc1f137a09320b5ef8
dengyh <dengyh071@gmail.com>
2026-01-09 10:56:50 +0800

    refactor: 回滚操作增加事务上下文 && 补充操作记录 --story=129873008

--- bkflow/apigw/views/rollback_template.py
+++ bkflow/apigw/views/rollback_template.py
@@ -21,14 +21,16 @@ import logging
 
 from apigw_manager.apigw.decorators import apigw_require
 from blueapps.account.decorators import login_exempt
+from django.db import transaction
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from bkflow.apigw.decorators import check_jwt_and_space, return_json_response
+from bkflow.constants import TemplateOperationSource, TemplateOperationType
 from bkflow.space.configs import FlowVersioning
 from bkflow.space.models import SpaceConfig
-from bkflow.template.models import Template, TemplateSnapshot
+from bkflow.template.models import Template, TemplateOperationRecord, TemplateSnapshot
 from bkflow.template.serializers.template import TemplateRollbackSerializer
 from bkflow.utils import err_code
 
@@ -42,11 +44,16 @@ logger = logging.getLogger("root")
 @check_jwt_and_space
 @return_json_response
 def rollback_template(request, space_id, template_id):
-    data = json.loads(request.body)
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError as e:
+        logger.error(f"[rollback_template] JSONDecodeError: {e}")
+        return JsonResponse({"result": False, "message": "请求体不是有效的 JSON 格式", "code": err_code.VALIDATION_ERROR.code})
+
     try:
         instance = Template.objects.get(id=template_id, space_id=space_id)
     except Template.DoesNotExist:
-        return JsonResponse({"result": False, "message": "Template not found"})
+        return JsonResponse({"result": False, "message": "Template not found", "code": err_code.VALIDATION_ERROR.code})
 
     if SpaceConfig.get_config(space_id=space_id, config_name=FlowVersioning.name) != "true":
         return JsonResponse({"result": False, "message": "当前空间未开启版本管理，无法回滚模板", "code": err_code.VALIDATION_ERROR.code})
@@ -61,7 +68,16 @@ def rollback_template(request, space_id, template_id):
     except TemplateSnapshot.DoesNotExist:
         return JsonResponse({"result": False, "message": f"版本 {version} 不存在", "code": err_code.VALIDATION_ERROR.code})
 
-    pipeline_tree = snapshot.data
-    draft_template = instance.update_draft_snapshot(pipeline_tree, request.user.username, version)
+    with transaction.atomic():
+        pipeline_tree = snapshot.data
+        draft_template = instance.update_draft_snapshot(pipeline_tree, request.user.username, version)
+
+    TemplateOperationRecord.objects.create(
+        operate_source=TemplateOperationSource.app.name,
+        operate_type=TemplateOperationType.rollback.name,
+        instance_id=instance.id,
+        operator=request.user.username,
+        extra_info={"version": version},
+    )
 
     return JsonResponse({"result": True, "data": draft_template.data, "code": err_code.SUCCESS.code})
