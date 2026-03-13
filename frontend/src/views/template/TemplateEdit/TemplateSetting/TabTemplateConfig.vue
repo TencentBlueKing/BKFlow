05db68086c1b8b5d8d01f3cf79171b106ea2ff8b
Mianhuatang8 <2542880657@qq.com>
2026-03-13 10:07:30 +0800

    feat: 支持HTTP回调 --story=132632590

--- frontend/src/views/template/TemplateEdit/TemplateSetting/TabTemplateConfig.vue
+++ frontend/src/views/template/TemplateEdit/TemplateSetting/TabTemplateConfig.vue
@@ -192,7 +192,18 @@ limitations under the License. */
             "
             @change="onTriggerConfigChange" />
         </section>
-
+        <section class="form-section http-callback-section">
+          <h4>
+            <span>{{ $t('HTTP回调') }}</span>
+            <span class="tip-desc">{{ $t('支持在流程结束（成功或失败）后，把任务输出的内容和执行结果作为参数调用回调接口') }}</span>
+          </h4>
+          <HttpCallbackConfig
+            ref="httpCallbackForm"
+            :webhook-data="formData.webhookConfigs"
+            :enable-webhook="enableWebhook"
+            :is-view-mode="isViewMode"
+            @change="onHttpCallbackChange" />
+        </section>
         <section class="form-section">
           <h4>{{ $t("其他") }}</h4>
           <!-- <bk-form-item v-if="!common" :label="$t('执行代理人')" data-test-id="tabTemplateConfig_form_executorProxy">
@@ -316,6 +327,7 @@ import NotifyTypeConfig from './NotifyTypeConfig.vue';
 import permission from '@/mixins/permission.js';
 import TimedTriggerConfig from './TimedTriggerConfig.vue';
 import LabelCascade from '../../../admin/Space/common/LabelCascade.vue';
+import HttpCallbackConfig from './HttpCallbackConfig.vue';
 
 export default {
     name: 'TabTemplateConfig',
@@ -324,6 +336,7 @@ export default {
         NotifyTypeConfig,
         TimedTriggerConfig,
         LabelCascade,
+        HttpCallbackConfig,
     },
     mixins: [permission],
     props: {
@@ -337,7 +350,7 @@ export default {
         isViewMode: Boolean,
     },
     data() {
-        const {
+          const {
             name,
             category,
             notify_type: notifyType,
@@ -347,83 +360,87 @@ export default {
             template_labels,
             default_flow_type,
             triggers,
+            webhook_configs: webhookConfigs,
+            enable_webhook: enableWebhook,
         } = this.$store.state.template;
 
         return {
-            formData: {
-                name,
-                category,
-                description,
-                executorProxy: execProxy ? [execProxy] : [],
-                receiverGroup: [],
-                notifyReceiver: notifyReceivers.more_receiver
-                    ? notifyReceivers.more_receiver.split(',')
-                    : [],
-                notifyType: notifyType
-                    ? [notifyType.success.slice(0), notifyType.fail.slice(0)]
-                    : [],
-                template_labels,
-                defaultFlowType: default_flow_type,
-                triggers,
-            },
-            stringLength: STRING_LENGTH,
-            rules: {
-                name: [
-                    {
-                        required: true,
-                        message: i18n.t('必填项'),
-                        trigger: 'blur',
-                    },
-                    {
-                        max: STRING_LENGTH.TEMPLATE_NAME_MAX_LENGTH,
-                        message:
-                            i18n.t('流程名称长度不能超过')
-                            + STRING_LENGTH.TEMPLATE_NAME_MAX_LENGTH
-                            + i18n.t('个字符'),
-                        trigger: 'blur',
-                    },
-                    {
-                        regex: NAME_REG,
-                        message: `${i18n.t('流程名称不能包含')}'‘"”$&<>${i18n.t('非法字符')}`,
-                        trigger: 'blur',
-                    },
-                ],
-            },
-            taskCategories: TASK_CATEGORIES,
-            labelDialogShow: false,
-            labelRules: {
-                color: [
-                    {
-                        required: true,
-                        message: i18n.t('必填项'),
-                        trigger: 'blur',
-                    },
-                ],
-                name: [
-                    {
-                        required: true,
-                        message: i18n.t('必填项'),
-                        trigger: 'blur',
-                    },
-                    {
-                        max: 50,
-                        message:
-                            i18n.t('标签名称不能超过') + 50 + i18n.t('个字符'),
-                        trigger: 'blur',
-                    },
-                    {
-                        validator: val => this.templateLabels.every(label => label.name !== val),
-                        message: i18n.t('标签已存在，请重新输入'),
-                        trigger: 'blur',
-                    },
-                ],
-            },
-            labelDetail: {},
-            colorDropdownShow: false,
-            colorList: LABEL_COLOR_LIST,
-            labelLoading: false,
-            proxyPlaceholder: '',
-            isAllowSetMultipleTrigger: false, // 允许设置多个触发器
+          formData: {
+              name,
+              category,
+              description,
+              executorProxy: execProxy ? [execProxy] : [],
+              receiverGroup: [],
+              notifyReceiver: notifyReceivers.more_receiver
+                  ? notifyReceivers.more_receiver.split(',')
+                  : [],
+              notifyType: notifyType
+                  ? [notifyType.success.slice(0), notifyType.fail.slice(0)]
+                  : [],
+              template_labels,
+              defaultFlowType: default_flow_type,
+              triggers,
+              webhookConfigs: tools.deepClone(webhookConfigs),
+          },
+          enableWebhook,
+          stringLength: STRING_LENGTH,
+          rules: {
+              name: [
+                  {
+                      required: true,
+                      message: i18n.t('必填项'),
+                      trigger: 'blur',
+                  },
+                  {
+                      max: STRING_LENGTH.TEMPLATE_NAME_MAX_LENGTH,
+                      message:
+                          i18n.t('流程名称长度不能超过')
+                          + STRING_LENGTH.TEMPLATE_NAME_MAX_LENGTH
+                          + i18n.t('个字符'),
+                      trigger: 'blur',
+                  },
+                  {
+                      regex: NAME_REG,
+                      message: `${i18n.t('流程名称不能包含')}'‘"”$&<>${i18n.t('非法字符')}`,
+                      trigger: 'blur',
+                  },
+              ],
+          },
+          taskCategories: TASK_CATEGORIES,
+          labelDialogShow: false,
+          labelRules: {
+              color: [
+                  {
+                      required: true,
+                      message: i18n.t('必填项'),
+                      trigger: 'blur',
+                  },
+              ],
+              name: [
+                  {
+                      required: true,
+                      message: i18n.t('必填项'),
+                      trigger: 'blur',
+                  },
+                  {
+                      max: 50,
+                      message:
+                          i18n.t('标签名称不能超过') + 50 + i18n.t('个字符'),
+                      trigger: 'blur',
+                  },
+                  {
+                      validator: val => this.templateLabels.every(label => label.name !== val),
+                      message: i18n.t('标签已存在，请重新输入'),
+                      trigger: 'blur',
+                  },
+              ],
+          },
+          labelDetail: {},
+          colorDropdownShow: false,
+          colorList: LABEL_COLOR_LIST,
+          labelLoading: false,
+          proxyPlaceholder: '',
+          isAllowSetMultipleTrigger: false, // 允许设置多个触发器
         };
     },
     computed: {
@@ -465,8 +482,12 @@ export default {
         }
     },
     methods: {
-        ...mapMutations('template/', ['setTplConfig']),
-        ...mapActions('project', ['getProjectConfig', 'createTemplateLabel']),
+        ...mapMutations('template/', [
+          'setTplConfig'
+        ]),
+        ...mapActions('project', [
+          'getProjectConfig', 'createTemplateLabel'
+        ]),
         ...mapActions('spaceConfig/', [
             'getNotAuthSpaceConfig',
             'checkSpaceConfig',
@@ -517,7 +538,7 @@ export default {
             window.open(href, '_blank');
         },
         getTemplateConfig() {
-            const {
+          const {
                 name,
                 category,
                 description,
@@ -528,20 +549,29 @@ export default {
                 template_labels,
                 defaultFlowType,
                 triggers,
-            } = this.formData;
-            return {
-                name,
-                category,
-                description,
-                template_labels,
-                executor_proxy:
-                    executorProxy.length === 1 ? executorProxy[0] : '',
-                more_receiver: notifyReceiver,
-                receiver_group: receiverGroup,
-                notify_type: { success: notifyType[0], fail: notifyType[1] },
-                default_flow_type: defaultFlowType,
-                triggers,
-            };
+                webhookConfigs,
+          } = this.formData;
+          if (webhookConfigs?.extra_info) {
+            webhookConfigs.extra_info.interval = typeof webhookConfigs.extra_info.interval !== 'number' ? parseInt(webhookConfigs.extra_info.interval) : webhookConfigs.extra_info.interval;
+            webhookConfigs.extra_info.retry_times = typeof webhookConfigs.extra_info.retry_times !== 'number' ? parseInt(webhookConfigs.extra_info.retry_times) : webhookConfigs.extra_info.retry_times;
+            webhookConfigs.extra_info.timeout = typeof webhookConfigs.extra_info.timeout !== 'number' ? parseInt(webhookConfigs.extra_info.timeout) : webhookConfigs.extra_info.timeout;
+            webhookConfigs.extra_info.headers = webhookConfigs.extra_info.headers.filter(item => item.key !== '');
+        }
+        return {
+            name,
+            category,
+            description,
+            template_labels,
+            executor_proxy:
+            executorProxy.length === 1 ? executorProxy[0] : '',
+            more_receiver: notifyReceiver,
+            receiver_group: receiverGroup,
+            notify_type: { success: notifyType[0], fail: notifyType[1] },
+            default_flow_type: defaultFlowType,
+            triggers,
+            webhook_configs: webhookConfigs,
+            enable_webhook: this.enableWebhook,
+          };
         },
         jumpProjectManagement() {
             if (this.isViewMode) return;
@@ -576,17 +606,27 @@ export default {
         onTriggerConfigChange(triggers) {
             this.formData.triggers = triggers;
         },
-        onSaveConfig() {
-            this.$refs.configForm.validate().then((result) => {
-                if (!result) {
-                    return;
-                }
-                const data = this.getTemplateConfig();
-                console.log(data, 'config data');
-                this.setTplConfig(data);
-                this.closeTab();
-                this.$emit('templateDataChanged');
-            });
+        onHttpCallbackChange(val, isEnableWebhook = false) {
+          if (isEnableWebhook) {
+              this.enableWebhook = val;
+          } else {
+              this.formData.webhookConfigs = val;
+          }
+      },
+      onSaveConfig() {
+        this.$refs.configForm.validate().then(async (result) => {
+            if (!result) {
+              return;
+            }
+            const validation = this.$refs.httpCallbackForm ? await this.$refs.httpCallbackForm.validate() : true;
+            if (!validation) {
+              return;
+            }
+            const data = this.getTemplateConfig();
+            this.setTplConfig(data);
+            this.closeTab();
+            this.$emit('templateDataChanged');
+          });
         },
         beforeClose() {
             if (this.isViewMode) {
@@ -823,3 +863,5 @@ export default {
     }
 }
 </style>
+
+
