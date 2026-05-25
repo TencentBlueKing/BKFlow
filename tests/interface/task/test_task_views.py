69a8b4ada6b840b7be961d204241d66de8196e9c
guohelu <19503896967@163.com>
2026-05-25 15:47:32 +0800

    fix: 修复管理员权限逻辑问题 --story=130003551 # Reviewed, transaction id: 80864

--- tests/interface/task/test_task_views.py
+++ tests/interface/task/test_task_views.py
@@ -363,7 +363,7 @@ class TestTaskInterfaceViewSet:
 
         TaskInterfaceViewSet._inject_user_task_auth(request, data)
 
-        assert "FLOW_MOCK" in data["data"]["auth"]
+        assert PermissionType.FLOW_MOCK.value in data["data"]["auth"]
 
     def test_inject_user_task_auth_result_false(self):
         """Test _inject_user_task_auth when result is False"""
