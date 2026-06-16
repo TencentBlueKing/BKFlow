17e3e4d39f0bda66d0f58b8ed18147e84b37d72d
guohelu <19503896967@163.com>
2026-06-16 18:05:58 +0800

    fix: 修改review问题 --story=135278474 # Reviewed, transaction id: 82119

--- bkflow/apigw/views/get_task_list.py
+++ bkflow/apigw/views/get_task_list.py
@@ -49,7 +49,7 @@ def get_task_list(request, space_id):
         "task_id_list": "id__in",
         "executor": "executor",
         "template_id": "template_id",
-        "is_child_taskflow": "false",
+        "is_child_taskflow": "is_child_taskflow",
     }
     for k, v in filter_map.items():
         if k in data:
