ad7ec59343bd5dbd7f5c3ea2d4564b11e0a0dc37
v_ghluguo <v_ghluguo@tencent.com>
2026-07-22 15:03:32 +0800

    feat: 补充批量获取模板版本信息 --story=135517916

--- bkflow/template/views/template.py
+++ bkflow/template/views/template.py
@@ -668,6 +668,20 @@ class TemplateViewSet(UserModelViewSet):
             raise APIResponseError(result["message"])
         return Response(result["data"])
 
+    @swagger_auto_schema(method="GET", operation_description="批量获取模板版本")
+    @action(methods=["GET"], detail=False, url_path="batch_get_template_version")
+    def batch_get_template_version(self, request, *args, **kwargs):
+        template_ids = request.GET.get("template_ids")
+        if not template_ids:
+            return Response(exception=True, data={"message": "template_ids is required"})
+        template_ids = template_ids.split(",")
+        template_objs = Template.objects.filter(id__in=template_ids)
+        data = [
+            {"template_id": template_obj.id, "name": template_obj.name, "version": template_obj.version}
+            for template_obj in template_objs
+        ]
+        return Response(data=data)
+
     @action(methods=["GET"], detail=True, url_path="get_draft_template")
     def get_draft_template(self, request, *args, **kwargs):
         template_obj = self.get_object()
@@ -686,8 +700,6 @@ class TemplateViewSet(UserModelViewSet):
         pipeline_tree = replace_subprocess_version(draft_snapshot.data, flow_version_config)
 
         data["pipeline_tree"] = pipeline_tree
-        if template_obj.subprocess_info:
-            data["subprocess_info"] = template_obj.subprocess_info
         return Response(data=data)
 
     @action(methods=["GET"], detail=True, url_path="calculate_version")
