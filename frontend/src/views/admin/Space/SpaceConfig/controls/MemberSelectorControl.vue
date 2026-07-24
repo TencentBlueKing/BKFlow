0d367b03f60f7df6f6f4fab75f116d839178f383
Mianhuatang8 <2542880657@qq.com>
2026-07-24 16:33:34 +0800

    feat: 空间配置改版 --story=132989396 # Reviewed, transaction id: 83508

---
+++ frontend/src/views/admin/Space/SpaceConfig/controls/MemberSelectorControl.vue
@@ -0,0 +1,34 @@
+<template>
+  <MemberSelect
+    :value="normalizedValue"
+    :placeholder="schema.placeholder || ''"
+    @change="$emit('change', $event)" />
+</template>
+<script>
+  import MemberSelect from '@/components/common/Individualization/MemberSelect.vue';
+  export default {
+    name: 'MemberSelectorControl',
+    components: {
+      MemberSelect,
+    },
+    model: {
+      prop: 'value',
+      event: 'change',
+    },
+    props: {
+      value: {
+        type: Array,
+        default: () => [],
+      },
+      schema: {
+        type: Object,
+        default: () => ({}) },
+      },
+    computed: {
+      normalizedValue() {
+        console.log('空间管理员-人员值', this.value);
+        return Array.isArray(this.value) ? this.value : [];
+      },
+    },
+  };
+</script>
